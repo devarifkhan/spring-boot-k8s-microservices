f2f59f0efbf9c07de762549db699a6c003d21beb
devarifkhan <arifcse209@gmail.com>
2025-01-30 16:04:59 +0600

    Implement loans microservice with CRUD operations and error handling

---
+++ loans/src/main/java/com/devarifkhan/loans/LoansApplication.java
@@ -0,0 +1,43 @@
+package com.devarifkhan.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.devarifkhan.loans.controller") })
+@EnableJpaRepositories("com.devarifkhan.loans.repository")
+@EntityScan("com.devarifkhan.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "ArifBank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ariful Islam",
+                        email = "aidevstack@gmail.com",
+                        url = "https://www.aidevstack.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.aidevstack.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "ArifBank Loans microservice REST API Documentation",
+                url = "https://www.aidevstack.com/swagger-ui.html"
+        )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
