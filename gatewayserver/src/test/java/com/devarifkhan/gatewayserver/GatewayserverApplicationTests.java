7dd4c6885e227147b04aafadd1e894a886bc1463
devarifkhan <arifcse209@gmail.com>
2025-02-02 19:40:00 +0600

    Add gateway server module with initial configuration and Maven wrapper

---
+++ gatewayserver/src/test/java/com/devarifkhan/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
