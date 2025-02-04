e6e660c61d30ec09ed517f0b3e4bf1205546587d
devarifkhan <arifcse209@gmail.com>
2025-02-04 20:01:37 +0600

    Update correlation ID and application description; add Keycloak role converter

--- gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
@@ -3,24 +3,34 @@ package com.devarifkhan.gatewayserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity.authorizeExchange(exchanges-> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/arifbank/accounts/**").authenticated()
-                .pathMatchers("/arifbank/cards/**").authenticated()
-                .pathMatchers("/arifbank/loans/**").authenticated())
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+                        .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
-        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return serverHttpSecurity.build();
+    }
+
+    private ReactiveJwtAuthenticationConverterAdapter grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
 }
