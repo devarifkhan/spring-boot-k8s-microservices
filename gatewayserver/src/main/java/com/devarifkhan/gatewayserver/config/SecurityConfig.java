34e3b44d6d49125d5affdc86ddc3a0d73a7c6818
devarifkhan <arifcse209@gmail.com>
2025-02-04 20:45:37 +0600

    Refactor grantedAuthoritiesExtractor method to use Converter for JWT authentication

--- gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.devarifkhan.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -25,7 +29,7 @@ public class SecurityConfig {
         return serverHttpSecurity.build();
     }
 
-    private ReactiveJwtAuthenticationConverterAdapter grantedAuthoritiesExtractor() {
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
         JwtAuthenticationConverter jwtAuthenticationConverter =
                 new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
