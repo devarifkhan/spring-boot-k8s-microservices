07ba7e2f150e726e083f2172364ec559717faaea
devarifkhan <arifcse209@gmail.com>
2025-02-04 20:28:34 +0600

    Update application references and documentation from EazyBank to ArifBank

--- gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/devarifkhan/gatewayserver/config/SecurityConfig.java
@@ -16,9 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
-                        .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
-                        .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
+                        .pathMatchers("/arifbank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/arifbank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/arifbank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
                         .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
--- loans/src/main/java/com/devarifkhan/loans/LoansApplication.java
+++ loans/src/main/java/com/devarifkhan/loans/LoansApplication.java
@@ -12,29 +12,29 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
-@EnableJpaRepositories("com.eazybytes.loans.repository")
-@EntityScan("com.eazybytes.loans.model")*/
+/*@ComponentScans({ @ComponentScan("com.devarifkhan.loans.controller") })
+@EnableJpaRepositories("com.devarifkhan.loans.repository")
+@EntityScan("com.devarifkhan.loans.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Loans microservice REST API Documentation",
-				description = "EazyBank Loans microservice REST API Documentation",
+				description = "ArifBank Loans microservice REST API Documentation",
 				version = "v1",
 				contact = @Contact(
-						name = "Madan Reddy",
-						email = "tutor@eazybytes.com",
-						url = "https://www.eazybytes.com"
+						name = "Ariful Islam",
+						email = "arifcse209@gmail.com",
+						url = "https://www.aidevstack.com"
 				),
 				license = @License(
 						name = "Apache 2.0",
-						url = "https://www.eazybytes.com"
+						url = "https://www.aidevstack.com"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
-				description = "EazyBank Loans microservice REST API Documentation",
-				url = "https://www.eazybytes.com/swagger-ui.html"
+				description = "ArifBank Loans microservice REST API Documentation",
+				url = "https://www.aidevstack.com/swagger-ui.html"
 		)
 )
 public class LoansApplication {
